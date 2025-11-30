afa0c086dc32d30db55b6dd3517aeea9619c798e
affanjr07 <153810825+affanjr07@users.noreply.github.com>
2025-11-30 22:57:23 +0700

    backend

---
+++ backend/controllers/authController.js
@@ -0,0 +1,42 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { findUserByEmail, createUser } from "../models/userModel.js";
+
+export const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const exist = await findUserByEmail(email);
+    if (exist) return res.status(400).json({ msg: "Email sudah terdaftar" });
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    await createUser(username, email, hashed);
+
+    res.json({ msg: "Register berhasil" });
+  } catch (err) {
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await findUserByEmail(email);
+    if (!user) return res.status(400).json({ msg: "Email tidak ditemukan" });
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.status(400).json({ msg: "Password salah" });
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.json({ msg: "Login berhasil", token });
+  } catch (err) {
+    res.status(500).json({ msg: "Server error" });
+  }
+};
